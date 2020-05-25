ea36ee211f5323a41cb636bae2d8d1a9961a8fd1
siddesh <siddusrb@gmail.com>
2020-05-25 21:20:40 +0530

    renamed package from vexpo.virtualexpo to nammaexpo

--- src/main/java/com/nammaexpo/VirtualExpoApplication.java
+++ src/main/java/com/nammaexpo/VirtualExpoApplication.java
@@ -1,4 +1,4 @@
-package com.vexpo.virtualexpo;
+package com.nammaexpo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/nammaexpo/VirtualExpoApplicationTests.java
+++ src/test/java/com/nammaexpo/VirtualExpoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.vexpo.virtualexpo;
+package com.nammaexpo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
