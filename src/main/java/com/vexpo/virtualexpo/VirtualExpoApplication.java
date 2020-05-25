cb853ad7b0343904d12d3cd135e0c7912e769b88
siddesh <siddusrb@gmail.com>
2020-05-25 15:14:36 +0530

    initial project creation

---
+++ src/main/java/com/vexpo/virtualexpo/VirtualExpoApplication.java
@@ -0,0 +1,13 @@
+package com.vexpo.virtualexpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualExpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualExpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vexpo/virtualexpo/VirtualExpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vexpo.virtualexpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualExpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
