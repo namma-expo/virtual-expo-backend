205ad676785a8557d506bcc8b7691c4180e20d64
Basavesh <basavesh1308@gmail.com>
2020-05-29 19:37:01 +0530

    added loadUserByUsername logic for user Authenticaton

--- src/main/java/com/nammaexpo/controllers/RegistrationController.java
+++ src/main/java/com/nammaexpo/controllers/RegistrationController.java
@@ -29,7 +29,6 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
-@Controller
 public class RegistrationController {
 
     @Autowired
