503f8ad2fe382281287e09d40ff46311ecc65714
Basavesh <basavesh1308@gmail.com>
2020-05-31 17:37:50 +0530

    included DataSourceAutoConfiguration and resolved repo bean problem

--- src/main/java/com/nammaexpo/VirtualExpoApplication.java
+++ src/main/java/com/nammaexpo/VirtualExpoApplication.java
@@ -5,12 +5,9 @@ package com.nammaexpo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
 public class VirtualExpoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VirtualExpoApplication.class, args);
