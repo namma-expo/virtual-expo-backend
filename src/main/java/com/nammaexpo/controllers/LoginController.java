248aedc574e40fa0f2162e11118a1b9b0486e12d
Manoj Gaonkar <manoj.aricent@PP-C02NK7FSG3QD.local>
2020-06-14 19:30:02 +0530

    Swagger added custom configuration for APIs

--- src/main/java/com/nammaexpo/controllers/LoginController.java
+++ src/main/java/com/nammaexpo/controllers/LoginController.java
@@ -2,25 +2,30 @@ package com.nammaexpo.controllers;
 
 import com.nammaexpo.expection.ExpoException;
 import com.nammaexpo.expection.ExpoException.ErrorCode;
+import com.nammaexpo.models.ErrorResponse;
 import com.nammaexpo.models.ExpoUserDetails;
 import com.nammaexpo.payload.request.LoginRequest;
 import com.nammaexpo.payload.response.JwtResponse;
 import com.nammaexpo.persistance.dao.UserRepository;
 import com.nammaexpo.services.ExpoUserDetailsService;
 import com.nammaexpo.utils.JwtUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "Login Controller")
 @Slf4j
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -41,9 +46,14 @@ public class LoginController {
   @Autowired
   private PasswordEncoder encoder;
 
-  @RequestMapping("/authenticate")
+  @ApiOperation(value = "Login User")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Authenticated", response = JwtResponse.class),
+      @ApiResponse(code = 403, message = "Authentication Failed", response = ErrorResponse.class)
+  })
+  @PostMapping("/authenticate")
   public ResponseEntity<JwtResponse> authenticateUser(
-      @RequestBody LoginRequest loginRequest) throws Exception {
+      @RequestBody LoginRequest loginRequest) {
 
     try {
       authenticationManager.authenticate(
