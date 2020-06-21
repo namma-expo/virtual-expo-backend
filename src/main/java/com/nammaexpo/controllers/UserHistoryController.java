74490ad8f71757e450d318cbb7b818ae1b887639
Manoj Gaonkar <manoj.aricent@PP-C02NK7FSG3QD.local>
2020-06-21 10:27:52 +0530

    User History APIs added

--- src/main/java/com/nammaexpo/controllers/UserHistoryController.java
+++ src/main/java/com/nammaexpo/controllers/UserHistoryController.java
@@ -1,9 +1,23 @@
 package com.nammaexpo.controllers;
 
+import com.nammaexpo.expection.ErrorCode;
+import com.nammaexpo.expection.ExpoException;
+import com.nammaexpo.models.enums.MessageCode;
+import com.nammaexpo.models.enums.UserAction;
+import com.nammaexpo.payload.response.MessageResponse;
+import com.nammaexpo.payload.response.UserHistoryResponse;
+import com.nammaexpo.persistance.dao.UserHistoryRepository;
+import com.nammaexpo.persistance.dao.UserRepository;
+import com.nammaexpo.persistance.model.UserEntity;
+import com.nammaexpo.persistance.model.UserHistoryEntity;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Api(tags = "User History Controller")
 @Slf4j
@@ -11,4 +25,60 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserHistoryController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserHistoryRepository userHistoryRepository;
+
+    @PostMapping("/user/history/{action}/{exhibitionId}")
+    public MessageResponse createUserHistory(
+            @PathVariable("action") UserAction userAction,
+            @PathVariable("exhibitionId") int exhibitionId,
+            @RequestHeader(value = "Authorization") String authorization
+    ) {
+
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        UserEntity userEntity = userRepository.findByEmail(userName)
+                .orElseThrow(() -> ExpoException.error(
+                        ErrorCode.TRANSACTION_NOT_FOUND)
+                );
+
+        userHistoryRepository.save(UserHistoryEntity.builder()
+                .action(userAction)
+                .userId(userEntity.getId())
+                .exhibitionId(exhibitionId)
+                .build());
+
+        return MessageResponse.builder()
+                .messageCode(MessageCode.USER_HISTORY_LOGGED)
+                .build();
+    }
+
+
+    @GetMapping("/user/history")
+    public List<UserHistoryResponse> getUserHistory(
+            @RequestHeader(value = "Authorization") String authorization
+    ) {
+
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        UserEntity userEntity = userRepository.findByEmail(userName)
+                .orElseThrow(() -> ExpoException.error(
+                        ErrorCode.TRANSACTION_NOT_FOUND)
+                );
+
+        List<UserHistoryEntity> userHistoryEntities = userHistoryRepository.findByUserId(userEntity.getId());
+
+        return userHistoryEntities.stream()
+                .map(userHistoryEntity -> UserHistoryResponse.builder()
+                        .date(userHistoryEntity.getCreatedAt())
+                        .exhibitionId(userHistoryEntity.getExhibitionId())
+                        .userAction(userHistoryEntity.getAction())
+                        .build())
+                .collect(Collectors.toList());
+
+
+    }
 }
--- src/main/java/com/nammaexpo/persistance/dao/UserHistoryRepository.java
+++ src/main/java/com/nammaexpo/persistance/dao/UserHistoryRepository.java
@@ -4,7 +4,11 @@ import com.nammaexpo.persistance.model.UserHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserHistoryRepository extends JpaRepository<UserHistoryEntity, Integer> {
+
+    List<UserHistoryEntity> findByUserId(int userId);
 
 }
